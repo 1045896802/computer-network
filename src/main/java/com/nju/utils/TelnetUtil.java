184fc806e716f0f0cc3d998fd3f75bd4b6236b23
Maxyu <1045896802@qq.com>
2021-01-07 09:37:48 +0800

    前端修改为3个button

--- src/main/java/com/nju/utils/TelnetUtil.java
+++ src/main/java/com/nju/utils/TelnetUtil.java
@@ -14,52 +14,48 @@ public class TelnetUtil {
      */
     private TelnetConnection[] telnet;
 
-//    public TelnetUtil(@Value("${switch.ip}") String ip,
-//                      @Value("${switch.port}") int port,
-//                      @Value("${switch.user}") String user,
-//                      @Value("${switch.password}") String password,
-//                      @Value("${switch.enablePassword}") String enablePassword) {
-//        try {
-//            System.out.println(ip + " " + port + " " + user + " " + password);
-//            System.out.println("启动Telnet...");
-//            //telnet[0] = new TelnetConnection(ip, port, user, password, enablePassword);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    public TelnetUtil(@Value("#{'${router.ip:}'.split(',')}") String[] ip,
-                      @Value("${router.port}") int port,
-                      @Value("${router.user}") String user,
-                      @Value("${router.password}") String password,
-                      @Value("${router.enablePassword}") String enablePassword) {
+    public TelnetUtil(@Value("${switch.ip}") String ip,
+                      @Value("${switch.port}") int port,
+                      @Value("${switch.user}") String user,
+                      @Value("${switch.password}") String password,
+                      @Value("${switch.enablePassword}") String enablePassword) {
         try {
-            telnet = new TelnetConnection[ip.length];
+            System.out.println(ip + " " + port + " " + user + " " + password);
             System.out.println("启动Telnet...");
-            for (int i = 0; i < ip.length; i++) {
-                System.out.println(ip[i] + " " + port + " " + user + " " + password + " " + enablePassword);
-                telnet[i] = new TelnetConnection(ip[i], port, user, password, enablePassword);
-            }
+            //telnet[0] = new TelnetConnection(ip, port, user, password, enablePassword);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+//    public TelnetUtil(@Value("#{'${router.ip:}'.split(',')}") String[] ip,
+//                      @Value("${router.port}") int port,
+//                      @Value("${router.user}") String user,
+//                      @Value("${router.password}") String password,
+//                      @Value("${router.enablePassword}") String enablePassword) {
+//        try {
+//            telnet = new TelnetConnection[ip.length];
+//            System.out.println("启动Telnet...");
+//            for (int i = 0; i < ip.length; i++) {
+//                System.out.println(ip[i] + " " + port + " " + user + " " + password + " " + enablePassword);
+//                telnet[i] = new TelnetConnection(ip[i], port, user, password, enablePassword);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     public String sendCommand(String router, String command) {
         String s = "";
         System.out.println("router:" + router + ",command:" + command);
         if ("router1".equals(router)) {
+            telnet[0].sendCommand(command);
             s = telnet[0].sendCommand(command);
         } else if ("router2".equals(router)) {
+            telnet[1].sendCommand(command);
             s = telnet[1].sendCommand(command);
         } else if ("router3".equals(router)) {
-            s = telnet[2].sendCommand(command);
-        }
-        if ("router1".equals(router)) {
-            s = telnet[0].sendCommand(command);
-        } else if ("router2".equals(router)) {
-            s = telnet[1].sendCommand(command);
-        } else if ("router3".equals(router)) {
+            telnet[2].sendCommand(command);
             s = telnet[2].sendCommand(command);
         }
         System.out.println(s);
